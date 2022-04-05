90ab66d930929a226ca09f4d7168351fcea73756
rushayhays <rushayhays@outlook.com>
2022-04-05 14:34:07 -0500

    the first bit

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import './index.css';
 // import reportWebVitals from './reportWebVitals';
 import { Kennel } from "./components/Kennel.js"
 
-ReactDOM.render(
-  <React.StrictMode>
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
     <Kennel />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
