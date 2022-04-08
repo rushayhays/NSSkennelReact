f56f212c48ed22a7195e07d0111e2c76b9b81ad1
rushayhays <rushayhays@outlook.com>
2022-04-08 09:41:05 -0500

    animal spotlight feature added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { PropsAndState } from './PropsAndState'
+import { AnimalSpotlight } from "./animal/AnimalSpotlight";
+import { getRandomId } from "../modules/AnimalManager";
 
-export const Home = () => (
-    <>
-        <h2>Nashville Kennels</h2>
-        <small>Loving care when you're not there.</small>
 
+export const Home = () => {
+    const [spotlightId, setSpotlightId] = useState(0);
+
+    const refreshSpotlightAnimal = () => {
+      getRandomId().then(setSpotlightId);
+    };
+  
+    useEffect(() => {
+      refreshSpotlightAnimal();
+    }, []);
+  
+    return (
+      <>
         <address>
-            <div>Visit Us at the Nashville North Location</div>
-            <div>500 Puppy Way</div>
+          Visit Us at the Nashville North Location
+          <br />
+          500 Puppy Way
         </address>
-        <PropsAndState yourName={"Brenda"} />
-    </>
-)
+        <h1>Animal Spotlight</h1>
+        <button onClick={refreshSpotlightAnimal}>Reload &#x27f3;</button>
+        {
+          spotlightId && <AnimalSpotlight animalId={spotlightId} />
+        }
+      </>
+    );
+  };
